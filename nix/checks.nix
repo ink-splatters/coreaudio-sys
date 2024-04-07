e7ff3bb17918a8baf4c0968d6245bf085c1fabc1
Peter A. <ink.splatters@pm.me>
2024-04-07 20:48:48 +0200

    linting errors

--- nix/checks.nix
+++ nix/checks.nix
@@ -6,7 +6,11 @@ pre-commit-hooks.lib.${system}.run {
     # clang-format.enable = true;
     # clang-tidy.enable = true;
     deadnix.enable = true;
-    markdownlint.enable = true;
+
+    markdownlint = {
+	enable = true;
+	configuration.settings = { MD013.line_length = 500; };
+    };
 
     nil.enable = true;
     nixfmt.enable = true;
