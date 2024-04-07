e6a0f81a955c0e23f308e1ddd3497e9d9c9f5226
Peter A. <ink.splatters@pm.me>
2024-04-07 21:02:49 +0200

    nix: more frameworks

--- nix/common.nix
+++ nix/common.nix
@@ -4,7 +4,6 @@ with darwin.apple_sdk.frameworks; {
   RUSTFLAGS = lib.optionalString ("${system}" == "aarch64-darwin")
     "-Ctarget-cpu=apple-m1 " + "-C link-arg=-fuse-ld=lld";
 
-  buildInputs = [ iconv AudioUnit CoreAudio xcrun ];
-
-  COREAUDIO_SDK_PATH = "${CoreAudio}";
+  buildInputs = [ iconv ]
+    ++ [ AudioUnit AudioToolbox CoreMIDI OpenAL CoreAudio ];
 }
